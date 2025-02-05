2848188388507a00f4daf12d49a22af2f6294644
Osama Yeslam <os7softgithub@gmail.com>
2025-02-05 09:59:53 +0300

    feat: add Category handler and queries

---
+++ EShopPro.Application/Features/Categories/Handlers/GetAllCategoriesHandlers.cs
@@ -0,0 +1,19 @@
+﻿using EShopPro.Application.Features.Categories.DTOs;
+using EShopPro.Application.Features.Categories.Queries;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShopPro.Application.Features.Categories.Handlers
+{
+    public class GetAllCategoriesHandlers : IRequestHandler<GetAllCategoriesQueries, List<CategoryDto>>
+    {
+        public Task<List<CategoryDto>> Handle(GetAllCategoriesQueries request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EShopPro.Application/Features/Categories/Queries/GetAllCategoryQueries.cs
@@ -0,0 +1,14 @@
+﻿using EShopPro.Application.Features.Categories.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShopPro.Application.Features.Categories.Queries
+{
+    public class GetAllCategoriesQueries : IRequest<List<CategoryDto>>
+    {
+    }
+}
